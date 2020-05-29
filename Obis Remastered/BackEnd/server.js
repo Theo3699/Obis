24d760a2912e11836ac175a024ba474d47da8daa
Theo3699 <61683286+Theo3699@users.noreply.github.com>
2020-05-29 18:13:13 +0300

    Backend in progress

---
+++ Obis Remastered/BackEnd/server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+http
+    .createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        const headers = {
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Methods" : "POST, GET, OPTIONS",
+            "Access-Control-Max-Age" : 86400,
+            "Content-Type": "application/json"
+        };
+        res.writeHead(200, headers);
+
+
+    }).listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:3000`)
+    })
