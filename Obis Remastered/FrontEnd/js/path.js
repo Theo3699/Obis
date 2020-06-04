aaa981f9b1a2eb01506b2ac5c2ca80698f3da84a
emilian955 <emilian955@gmail.com>
2020-06-04 13:21:17 +0300

    Mihaila
    script de parcurgere diferite iframe-uri
    responsive la index si anumite cazuri particulare pentru overflow, logo si header. Alinierea header-ului
    Responsive la tabel cu disparitia anumitor coloane

---
+++ Obis Remastered/FrontEnd/js/path.js
@@ -0,0 +1,14 @@
+const goToTable = () => {
+    const iframe = document.querySelector('#content');
+    iframe.src = 'html/tabel.html';
+}
+
+const goToBarChart=()=>{
+    const iframe=document.querySelector('#content');
+    iframe.src='html/grafic.html';
+}
+
+const goToLineChart=()=>{
+    const iframe=document.querySelector('#content');
+    iframe.src='html/grafic1.html';
+}
