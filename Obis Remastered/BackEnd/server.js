9b670b99e3bb834517c12361de3a337f71ea8448
Theodor Samson <61683286+Theo3699@users.noreply.github.com>
2020-06-04 19:15:38 +0300

    Update
    Added transition from homepage to info and updated the server

--- Obis Remastered/BackEnd/server.js
+++ Obis Remastered/BackEnd/server.js
@@ -1,7 +1,10 @@
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
 const hostname = "127.0.0.1";
 const port = 3000;
+const handle = require("./handler");
 
 http
     .createServer((req, res) => {
@@ -14,7 +17,48 @@ http
             "Content-Type": "application/json"
         };
         res.writeHead(200, headers);
-        res.end("Hello from server!");
+//        res.end("Hello from server!");
+
+        let dataObject = [];
+        if (url.parse(req.url).pathname.startsWith("/download")){
+            const urlPath = url.parse(req.url).pathname;
+            if (urlPath === "/download/csv/data") {
+                const filePath = "./csv/data.csv";
+
+                fs.readFile(filePath, (err, content) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.end(content);
+                });
+            }
+        }
+        else {
+            req.on("error", (err) => {
+                console.error(err);
+            })
+            .on("data", (chunk) => {
+                dataObject.push(chunk);
+            })
+            .on("end", () => {
+                //body
+                dataObject = Buffer.concat(dataObject).toString();
+                if (dataObject) {
+                    dataObject = JSON.parse(dataObject);
+                }
+                else {
+                    //query string object
+                    dataObject = url.parse(req.url, true).query;
+                }
+                console.log(dataObject);
+
+                handle(req, dataObject).then((doc) => {
+                    responseObject = JSON.stringify(doc);
+
+                    res.end(responseObject);
+                });
+            });
+        }
 
 
     }).listen(port, hostname, () => {
