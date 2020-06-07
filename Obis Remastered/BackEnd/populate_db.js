f5a9b6579d60bac7f00b8cfbfec3707521c06c31
MehaiIulian <iulicamihaita@gmail.com>
2020-06-07 14:26:15 +0300

    Update populate_db.js

--- Obis Remastered/BackEnd/populate_db.js
+++ Obis Remastered/BackEnd/populate_db.js
@@ -1,121 +1,43 @@
 var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/obis", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
-    console.log("connected successfully to database");
+  console.log("connected successfully to database");
 });
 
 var countrySchema = new mongoose.Schema({
-    name: String,
-    year: Number,
+  name: String,
+  year: Number,
 }).index(
-    {
-        name: 1,
-        year: 1,
-    },
-    {
-        unique: true,
-    }
+  {
+    name: 1,
+    year: 1,
+  },
+  {
+    unique: true,
+  }
 );
 var Country = mongoose.model("Country", countrySchema);
 
 var dataSchema = new mongoose.Schema({
-    yearGrowth: Number,
-    maleGrowth: Number,
-    femaleGrowth: Number,
-    curePercentage: Number,
-    _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Country",
-    },
+  yearGrowth: Number,
+  maleGrowth: Number,
+  femaleGrowth: Number,
+  curePercentage: Number,
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Country",
+  },
 });
 var Data = mongoose.model("Data", dataSchema);
 
 
-/*function createCountry(countryObj) {
-    return new Promise((resolve) => {
-      if (!isValidCountry(countryObj)) {
-        resolve("error");
-      } else {
-        const newCountry = new Country(countryObj);
-        newCountry
-          .save()
-          .then((doc) => {//we get acces to the document that we just saved in our DB
-            resolve(doc);
-          })
-          .catch((err) => {
-            resolve(err);
-          });
-      }
-    });
-  }
-  
-  function createData(data) {
-    return new Promise((resolve) => {
-      if (!isValidData(data)) {
-        resolve("error");//datele date nu sunt valide
-      } else {
-        const newData = new Data({
-          yearGrowth: data.yearGrowth,
-          maleGrowth: data.maleGrowth,
-          curePercentage: data.curePercentage,
-          femaleGrowth: data.femaleGrowth,
-          _id: data.id,
-        });
-        newData
-          .save()
-          .then((doc) => {
-            resolve(doc);//the response
-          })
-          .catch((err) => {//any error that shows up
-            resolve(err.message);
-          });
-      }
-    });
-  }
-  */
-  function getCountries() {
-    return Country.find();//return all coutries from database
-  }
-  
-  function isValidCountry(country) {
-    if (country.name === undefined) {
-      return false;//test name
-    }
-    if (country.year === undefined) {
-      return false;//test year
-    }
-  
-    return true;
-  }
-  
-  
-  function isValidData(data) {
-    console.log(data);
-  
-    if (data.yearGrowth === undefined) {
-      return false;
-    }
-  
-    if (data.maleGrowth === undefined) {
-      return false;
-    }
-  
-    if (data.femaleGrowth === undefined) {
-      return false;
-    }
-  
-    if (data.curePercentage === undefined) {
-      return false;
-    }
-    return true;
-  }
 
 
 
@@ -124,202 +46,323 @@ var Data = mongoose.model("Data", dataSchema);
 
 
 const countryObjects = [
-    {
-        name: "alabama",
-        year: 2018,
-    },
-    {
-        name: "alabama",
-        year: 2017,
-    },
-    {
-        name: "alabama",
-        year: 2016,
-    },
-    {
-        name: "alabama",
-        year: 2015,
-    },
-    {
-        name: "alabama",
-        year: 2014,
-    },
-    {
-        name: "alaska",
-        year: 2018,
-    },
-    {
-        name: "alaska",
-        year: 2017,
-    },
-    {
-        name: "alaska",
-        year: 2016,
-    },
-    {
-        name: "alaska",
-        year: 2015,
-    },
-    {
-        name: "alaska",
-        year: 2014,
-    },
+  {
+    name: "alabama",
+    year: 2018,
+  },
+  {
+    name: "alabama",
+    year: 2017,
+  },
+  {
+    name: "alabama",
+    year: 2016,
+  },
+  {
+    name: "alabama",
+    year: 2015,
+  },
+  {
+    name: "alabama",
+    year: 2014,
+  },
+  {
+    name: "alaska",
+    year: 2018,
+  },
+  {
+    name: "alaska",
+    year: 2017,
+  },
+  {
+    name: "alaska",
+    year: 2016,
+  },
+  {
+    name: "alaska",
+    year: 2015,
+  },
+  {
+    name: "alaska",
+    year: 2014,
+  },
 
 
-    {
-      name: "California",
-      year: 2018,
+
+
+
+  {
+    name: "michigan",
+    year: 2018,
   },
   {
-      name: "California",
-      year: 2017,
+    name: "michigan",
+    year: 2017,
   },
   {
-      name: "California",
-      year: 2016,
+    name: "michigan",
+    year: 2016,
   },
   {
-      name: "California",
-      year: 2015,
+    name: "michigan",
+    year: 2015,
   },
   {
-      name: "California",
-      year: 2014,
+    name: "michigan",
+    year: 2014,
+  },
+  {
+    name: "arkansas",
+    year: 2018,
+  },
+  {
+    name: "arkansas",
+    year: 2017,
+  },
+  {
+    name: "arkansas",
+    year: 2016,
+  },
+  {
+    name: "arkansas",
+    year: 2015,
+  },
+  {
+    name: "arkansas",
+    year: 2014,
+  },
+
+
+
+
+  {
+    name: "California",
+    year: 2018,
+  },
+  {
+    name: "California",
+    year: 2017,
+  },
+  {
+    name: "California",
+    year: 2016,
+  },
+  {
+    name: "California",
+    year: 2015,
+  },
+  {
+    name: "California",
+    year: 2014,
   },
 
 
   {
     name: "Colorado",
     year: 2018,
-},
-{
+  },
+  {
     name: "Colorado",
     year: 2017,
-},
-{
+  },
+  {
     name: "Colorado",
     year: 2016,
-},
-{
+  },
+  {
     name: "Colorado",
     year: 2015,
-},
-{
+  },
+  {
     name: "Colorado",
     year: 2014,
-},
-    
+  },
+
 ];
 
 const dataObjects = [
-    {
-        yearGrowth: -0.7,
-        maleGrowth: -0.5,
-        curePercentage: 0.7,
-        femaleGrowth: 0.4,
-        //2018
-    },
-    {
-        yearGrowth: 1.5,
-        maleGrowth: -1.1,
-        curePercentage: 0,
-        femaleGrowth: 2.2,
-        //2017
-    },
-    {
-        yearGrowth: 0.3,
-        maleGrowth: 1.3,
-        curePercentage: 0,
-        femaleGrowth: -1.2,
-        //2016
-    },
-    {
-        yearGrowth: 0.3,
-        maleGrowth: 0.8,
-        curePercentage: 0,
-        femaleGrowth: 3.5,
-        //2015
-    },
-    {
-        yearGrowth: 0.6,
-        maleGrowth: 3.2,
-        curePercentage: 0,
-        femaleGrowth: -1.6,
-        //2014 Alabama
-    },
-    {
-        yearGrowth: -4.7,
-        maleGrowth: -2.2,
-        curePercentage: 4.7,
-        femaleGrowth: -7.6,
-        //2018 Alaska
-    },
-    {
-        yearGrowth: 2.8,
-        maleGrowth: 0.3,
-        curePercentage: 0,
-        femaleGrowth: 5.8,
-        //2017 Alaska
-    },
-    {
-        yearGrowth: 1.6,
-        maleGrowth: 3,
-        curePercentage: 0,
-        femaleGrowth: -0.2,
-        //2016 Alaska
-    },
-    {
-        yearGrowth: 0.1,
-        maleGrowth: 0,
-        curePercentage: 0,
-        femaleGrowth: 0.3,
-        //2015 Alaska
-    },
-    {
-        yearGrowth: 1.3,
-        maleGrowth: 0.1,
-        curePercentage: 0,
-        femaleGrowth: 2.7,
-        //2014 Alaska
-    },
+  {
+    yearGrowth: -0.7,
+    maleGrowth: -0.5,
+    curePercentage: 0.7,
+    femaleGrowth: 0.4,
+    //2018 Alabama
+  },
+  {
+    yearGrowth: 1.5,
+    maleGrowth: -1.1,
+    curePercentage: 0,
+    femaleGrowth: 2.2,
+    //2017
+  },
+  {
+    yearGrowth: 0.3,
+    maleGrowth: 1.3,
+    curePercentage: 0,
+    femaleGrowth: -1.2,
+    //2016
+  },
+  {
+    yearGrowth: 0.3,
+    maleGrowth: 0.8,
+    curePercentage: 0,
+    femaleGrowth: 3.5,
+    //2015
+  },
+  {
+    yearGrowth: 0.6,
+    maleGrowth: 3.2,
+    curePercentage: 0,
+    femaleGrowth: -1.6,
+    //2014 Alabama
+  },
+  {
+    yearGrowth: -4.7,
+    maleGrowth: -2.2,
+    curePercentage: 4.7,
+    femaleGrowth: -7.6,
+    //2018 Alaska
+  },
+  {
+    yearGrowth: 2.8,
+    maleGrowth: 0.3,
+    curePercentage: 0,
+    femaleGrowth: 5.8,
+    //2017 Alaska
+  },
+  {
+    yearGrowth: 1.6,
+    maleGrowth: 3,
+    curePercentage: 0,
+    femaleGrowth: -0.2,
+    //2016 Alaska
+  },
+  {
+    yearGrowth: 0.1,
+    maleGrowth: 0,
+    curePercentage: 0,
+    femaleGrowth: 0.3,
+    //2015 Alaska
+  },
+  {
+    yearGrowth: 1.3,
+    maleGrowth: 0.1,
+    curePercentage: 0,
+    femaleGrowth: 2.7,
+    //2014 Alaska
+  },
 
 
 
 
-    {
-      yearGrowth: 1.3,
-      maleGrowth: 1.5,
-      curePercentage: 0,
-      femaleGrowth: -0.2,
-      //2018 California
+  {
+    yearGrowth: 0.7,
+    maleGrowth: 0,
+    curePercentage: 0,
+    femaleGrowth: 1.4,
+    //2018 Michigan
   },
   {
-      yearGrowth:0.2,
-      maleGrowth: -0.4,
-      curePercentage: 0,
-      femaleGrowth: 0.6,
-      //2017 California
+    yearGrowth: -0.2,
+    maleGrowth: -1,
+    curePercentage: 0.2,
+    femaleGrowth: 0.6,
+    //2017
   },
   {
-      yearGrowth: 1.9,
-      maleGrowth: 0.3,
-      curePercentage: 0,
-      femaleGrowth: 1.5,
-      //2016 California
+    yearGrowth: 1.3,
+    maleGrowth: 0.7,
+    curePercentage: 0,
+    femaleGrowth: 2,
+    //2016
   },
   {
-      yearGrowth:-1.2,
-      maleGrowth: 0.6,
-      curePercentage: 1.2,
-      femaleGrowth: -1.7,
-      //2015 California
+    yearGrowth: 0.5,
+    maleGrowth: 0.8,
+    curePercentage: 0,
+    femaleGrowth: 0,
+    //2015
   },
   {
-      yearGrowth: 1.2,
-      maleGrowth: -1.1,
-      curePercentage: 0,
-      femaleGrowth: 2.3,
-      //2014 California
+    yearGrowth: -0.8,
+    maleGrowth: -0.2,
+    curePercentage: 0.8,
+    femaleGrowth: -1.3,
+    //2014 Michigan
+  },
+  {
+    yearGrowth: 2.1,
+    maleGrowth: -0.2,
+    curePercentage: 0.7,
+    femaleGrowth: 2,
+    //2018 Arkansas
+  },
+  {
+    yearGrowth: 1.5,
+    maleGrowth: -0.7,
+    curePercentage: 0,
+    femaleGrowth: -2,
+    //2017
+  },
+  {
+    yearGrowth: 1.2,
+    maleGrowth: -3,
+    curePercentage: 0,
+    femaleGrowth: 5.5,
+    //2016
+  },
+  {
+    yearGrowth: -1.4,
+    maleGrowth: -0.7,
+    curePercentage: 0,
+    femaleGrowth: -2.1,
+    //2015
+  },
+  {
+    yearGrowth: 1.3,
+    maleGrowth: 0.6,
+    curePercentage: 0,
+    femaleGrowth: 1.9,
+    //2014 Arkansas
+  },
+
+
+
+
+  {
+    yearGrowth: 1.3,
+    maleGrowth: 1.5,
+    curePercentage: 0,
+    femaleGrowth: -0.2,
+    //2018 California
+  },
+  {
+    yearGrowth: 0.2,
+    maleGrowth: -0.4,
+    curePercentage: 0,
+    femaleGrowth: 0.6,
+    //2017 California
+  },
+  {
+    yearGrowth: 1.9,
+    maleGrowth: 0.3,
+    curePercentage: 0,
+    femaleGrowth: 1.5,
+    //2016 California
+  },
+  {
+    yearGrowth: -1.2,
+    maleGrowth: 0.6,
+    curePercentage: 1.2,
+    femaleGrowth: -1.7,
+    //2015 California
+  },
+  {
+    yearGrowth: 1.2,
+    maleGrowth: -1.1,
+    curePercentage: 0,
+    femaleGrowth: 2.3,
+    //2014 California
   },
 
 
@@ -330,35 +373,35 @@ const dataObjects = [
     curePercentage: 0,
     femaleGrowth: 0.4,
     //2018 Colorado
-},
-{
+  },
+  {
     yearGrowth: 0.7,
     maleGrowth: -0.9,
     curePercentage: 0,
     femaleGrowth: 1.6,
     //2017 Colorado
-},
-{
+  },
+  {
     yearGrowth: 4.2,
     maleGrowth: 2.2,
     curePercentage: 0,
     femaleGrowth: 2.0,
     //2016 Colorado
-},
-{
-    yearGrowth:-2.2,
-    maleGrowth: 0-1.7,
+  },
+  {
+    yearGrowth: -2.2,
+    maleGrowth: 0 - 1.7,
     curePercentage: 2.2,
     femaleGrowth: -0.5,
     //2015 Colorado
-},
-{
+  },
+  {
     yearGrowth: 0,
     maleGrowth: 1.1,
     curePercentage: 0,
     femaleGrowth: -1.1,
     //2014 Colorado
-},
+  },
 
 
 ];
