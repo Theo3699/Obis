5753dabde282615acffbc30615a93d9bc6db26b8
MehaiIulian <iulicamihaita@gmail.com>
2020-06-07 14:10:48 +0300

    Frontfinished

--- Obis Remastered/FrontEnd/javascript/Chart.js
+++ Obis Remastered/FrontEnd/javascript/Chart.js
@@ -1,43 +1,92 @@
-const ctx = document.getElementById('barChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }],
-        },
-        responsive: true,
-        mantainAspectRatio: true,
-        legen: {
-            position: 'top'
-        }
+const apiURL = 'http://localhost:3000';
+let barChart = null;
+
+function populateFilters() {
+    fetch(`${apiURL}/data/country`).then(response => response.json()).then(data => {
+        const firstCountry = document.getElementById('country1');
+        const secondCountry = document.getElementById('country2');
+        data.forEach(({
+            _id,
+            name,
+            year
+        }) => {
+            const optionForFirstCountry = document.createElement('option');
+            const optionForSecondCountry = document.createElement('option');
+            optionForFirstCountry.setAttribute('value', _id);
+            optionForSecondCountry.setAttribute('value', _id);
+            optionForFirstCountry.appendChild(document.createTextNode(`${name} ${year}`));
+            optionForSecondCountry.appendChild(document.createTextNode(`${name} ${year}`));
+            firstCountry.appendChild(optionForFirstCountry);
+            secondCountry.appendChild(optionForSecondCountry);
+        });
+    });
+}
+
+function compareData() {
+    const country1 = document.getElementById('country1').value;
+    const country2 = document.getElementById('country2').value;
+    const criteriaOptions = [...document.getElementById('criteria').options];
+    const criteria = criteriaOptions.filter(option => option.selected).map(option => option.value);
+    const warning = document.getElementById('warning');
+    if (country1 !== country2 && criteria.length) {
+        warning.style.display = 'none';
+        fetch(`${apiURL}/compare?firstId=${country1}&secondId=${country2}&criterias=${criteria}`)
+            .then(response => response.json())
+            .then(data => {
+                createChart(data, criteria);
+            }).catch(error => console.log(error));
+    } else {
+        warning.style.display = 'block';
     }
-});
-//next to integrate
+}
+
+function createChart({
+    firstCountry,
+    secondCountry
+}, criteria) {
+    const ctx = document.getElementById('barChart').getContext('2d');
+    const data = {
+        labels: [...criteria],
+        datasets: [{
+            label: firstCountry.name,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            data: criteria.map(crit => firstCountry[crit])
+        },
+        {
+            label: secondCountry.name,
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 1)',
+            borderWidth: 1,
+            data: criteria.map(crit => secondCountry[crit])
+        }
+        ]
+    };
+    if (barChart) {
+        barChart.data = data;
+        barChart.update();
+    } else {
+        barChart = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: {
+                barValueSpacing: 20,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            min: 0
+                        }
+                    }],
+                },
+                responsive: true,
+                mantainAspectRatio: false,
+                legend: {
+                    position: 'top'
+                }
+            }
+        });
+    }
+}
+
+populateFilters();
--- Obis Remastered/FrontEnd/javascript/stats.js
+++ Obis Remastered/FrontEnd/javascript/stats.js
@@ -1,12 +1,77 @@
-const ctx = document.getElementById('lineChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: [{ x: 10, y: 20 }],
-    options: {
-        scales: {
-            yAxes: [{
-                stacked: true
-            }]
+const apiURL = 'http://localhost:3000';
+let lineChart = null;
+
+function populateFilter() {
+    fetch(`${apiURL}/data/country`).then(response => response.json()).then(data => {
+        const country = document.getElementById('country');
+        const countryNames = data.map(country => country.name);
+        const uniqueCountries = countryNames.filter((name, index) => index === countryNames.indexOf(name));
+        uniqueCountries.forEach((countryName) => {
+            const option = document.createElement('option');
+            option.setAttribute('value', countryName);
+            option.appendChild(document.createTextNode(countryName));
+            country.appendChild(option);
+        });
+    });
+}
+
+function fetchData() {
+    const selectedCountry = document.getElementById('country').value;
+    fetch(`${apiURL}/data?country=${selectedCountry}`)
+        .then(response => response.json())
+        .then(data => createChart(data))
+        .catch(err => console.log(err));
+}
+
+function createChart(data) {
+    const ctx = document.getElementById('lineChart').getContext('2d');
+    const chartData = {
+        labels: data.map(country => country.year),
+        datasets: [{
+            label: 'Year Growth',
+            data: data.map(country => country.yearGrowth),
+            borderColor: "peru",
+            fill: false
+        },
+        {
+            label: 'Male Growth',
+            data: data.map(country => country.maleGrowth),
+            borderColor: "aquamarine",
+            fill: false
+        },
+        {
+            label: 'Female Growth',
+            data: data.map(country => country.femaleGrowth),
+            borderColor: "plum",
+            fill: false
+        },
+        {
+            label: 'Cure percentage',
+            data: data.map(country => country.curePercentage),
+            borderColor: "olive",
+            fill: false
         }
+        ]
     }
-});
+    if (lineChart) {
+        lineChart.data = chartData;
+        lineChart.update();
+    } else {
+        lineChart = new Chart(ctx, {
+            type: 'line',
+            data: chartData,
+            options: {
+                scales: {
+                    yAxes: [{
+                        beginAtZero: true
+                    }]
+                },
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    }
+}
+
+populateFilter();
+fetchData();
